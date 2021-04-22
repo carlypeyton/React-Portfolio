d1e3ec34af6d191a2e3f1e043ca1032657032573
Carly Peyton <carlypeyton@gmail.com>
2021-04-22 15:38:36 -0400

    Add footer component and styling.

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+import GithubIcon from "../../assets/icons/Github.png";
+import LinkedInIcon from "../../assets/icons/LinkedIn.png";
+import FooterStyle from "./Footer.css"
+
+function Footer() {
+  return (
+    <footer className="footer-margin fixed-bottom card-footer bg-light footer-font-style text-muted footer-position" style={FooterStyle}>
+      <a href="https://github.com/carlypeyton">
+        <img src={GithubIcon} alt="Github Logo" className="icon"/></a>
+      <a href="https://www.linkedin.com/in/carly-p-5141841ba/">
+        <img src={LinkedInIcon} alt="LinkedIn Logo" className="icon"/></a>
+    </footer>
+  );
+}
+
+export default Footer;
