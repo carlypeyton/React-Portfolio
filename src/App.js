028a227a35e4c2d44d12956d9a8de9b4da83018c
Carly Peyton <carlypeyton@gmail.com>
2021-04-22 15:42:46 -0400

    Update App.js and installed axios for form submitting.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import About from "./components/pages/About";
-import Contact from "./components/pages/Contact";
-import Portfolio from "./components/pages/Blog";
+import NavBar from "./components/NavBar/NavBar";
+import Footer from "./components/Footer/Footer";
+import About from "./pages/About";
+import Portfolio from "./pages/Portfolio";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
     <Router>
     <div>
-      <Header />
+      <NavBar />
       <Route exact path="/" component={About} />
       <Route exact path="/about" component={About} />
       <Route exact path="/portfolio" component={Portfolio} />
