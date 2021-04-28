452966bf804e5a9c58c1be5379044d79d266d0c9
Carly Peyton <carlypeyton@gmail.com>
2021-04-28 18:50:41 -0400

    Fix typo.

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
       <Route exact path="/" component={About} />
       <Route exact path="/about" component={About} />
       <Route exact path="/portfolio" component={Portfolio} />
-      <Route path="/contact" component={Contact} />
+      <Route exact path="/contact" component={Contact} />
       <Footer />
     </div>
   </Router>
