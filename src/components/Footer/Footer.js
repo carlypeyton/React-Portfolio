420e848e3b0ec75ced11ba42e45fda480292fd1f
Carly Peyton <carlypeyton@gmail.com>
2021-05-27 21:06:26 -0400

    Update linked in url.

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -8,7 +8,7 @@ function Footer() {
     <footer className="footer-margin fixed-bottom card-footer bg-light footer-font-style text-muted footer-position" style={FooterStyle}>
       <a href="https://github.com/carlypeyton" target="_blank" rel="noreferrer">
         <img src={GithubIcon} alt="Github Logo" className="icon"/></a>
-      <a href="https://www.linkedin.com/in/carly-p-5141841ba/" target="_blank" rel="noreferrer">
+      <a href="https://www.linkedin.com/in/carlypeyton/" target="_blank" rel="noreferrer">
         <img src={LinkedInIcon} alt="LinkedIn Logo" className="icon"/></a>
     </footer>
   );
